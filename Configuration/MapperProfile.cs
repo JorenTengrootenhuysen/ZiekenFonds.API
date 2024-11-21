dff8185fa8fbf6e7a9f1d4ac51d92f2d3c90e9e8
JorenTengrootenhuysen <r0945159@student.thomasmore.be>
2024-11-21 19:04:11 +0100

    Automapper aangemaakt

---
+++ Configuration/MapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace ZiekenFonds.API.Configuration
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile() 
+        {
+            //Hier de mappings met CreateMap<>()
+        }
+    }
+}
