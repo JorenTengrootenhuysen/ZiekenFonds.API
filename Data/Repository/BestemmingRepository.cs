ffa2fef5cdbc2c1767cc822c30b748aa3f7f17c8
MaartenJanssens <r0841547@student.thomasmore.be>
2024-11-26 15:28:30 +0100

    Bestemming repo aangemaakt + onkosten aangepast

---
+++ Data/Repository/BestemmingRepository.cs
@@ -0,0 +1,11 @@
+﻿using ZiekenFonds.API.Models;
+
+namespace ZiekenFonds.API.Data.Repository
+{
+    public class BestemmingRepository : GenericRepository<Bestemming>, IBestemmingRepository
+    {
+        public BestemmingRepository(ZiekenFondsApiContext context) : base(context)
+        {
+        }
+    }
+}
