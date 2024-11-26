5b9fa4eb235429800e0b2926249a70798be73511
RensTengrootenhuysen <r0991312@student.thomasmore.be>
2024-11-26 15:27:49 +0100

    Foto repos aangemaakt

---
+++ Data/Repository/FotoRepository.cs
@@ -0,0 +1,11 @@
+﻿using ZiekenFonds.API.Models;
+
+namespace ZiekenFonds.API.Data.Repository
+{
+    public class FotoRepository : GenericRepository<Foto>
+    {
+        public FotoRepository(ZiekenFondsApiContext context) : base(context)
+        {
+        }
+    }
+}
