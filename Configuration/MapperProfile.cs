755a046b603e84777cbf7b7964fe065567f6a0c6
MaartenJanssens <r0841547@student.thomasmore.be>
2024-12-05 13:22:33 +0100

    Onkosten controller aangemaakt

--- Configuration/MapperProfile.cs
+++ Configuration/MapperProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using ZiekenFonds.API.Dto.Activiteit;
+using ZiekenFonds.API.Dto.Onkosten;
 using ZiekenFonds.API.Models;
 
 namespace ZiekenFonds.API.Configuration
@@ -12,6 +13,11 @@ namespace ZiekenFonds.API.Configuration
             CreateMap<Activiteit, ActiviteitOphalenDto>();
             CreateMap<ActiviteitMakenDto, Activiteit>();
             CreateMap<ActiviteitUpdateDto, Activiteit>();
+
+            //Onkosten 
+            CreateMap<Onkosten, GetOnkostenDto>();
+            CreateMap<CreateOnkostenDto, Onkosten>();
+            CreateMap<UpdateOnkostenDto, Onkosten>();
         }
     }
 }
